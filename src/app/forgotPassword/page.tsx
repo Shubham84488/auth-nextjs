46104f51e049435eb3fa466f443829cb3e165b40
Shubham84488 <134442202+Shubham84488@users.noreply.github.com>
2024-11-08 18:09:36 +0530

    changed some design

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -28,21 +28,24 @@ export default function ForgotPassword(){
     }
 
     return(
-        <div className="bg-gradient-to-br from-pink-100 via-white to-blue-200 w-4/5 mx-auto my-[100px]">
-            <div><Toaster/></div>
+        <div className="bg-gradient-to-br from-pink-100 via-white to-blue-200 h-[100vh]">
+            <div className=" w-4/5 mx-auto py-[100px]">
+                <div><Toaster/></div>
 
-            <label htmlFor="email" className="block mb-3">Enter your emailID below :</label>
-            <input type="text" className="p-2 w-2/5 border-black border-[3px] rounded-lg mb-4 " 
-            onChange={(e)=>setEmail(e.target.value)}
-            value={email}
-            id="email"
-            name="email" 
-            required/>
+                <label htmlFor="email" className="block mb-3">Enter your emailID below :</label>
+                <input type="text" className="p-2 w-2/5 border-black border-[3px] rounded-lg mb-4 " 
+                onChange={(e)=>setEmail(e.target.value)}
+                value={email}
+                id="email"
+                name="email" 
+                required/>
 
-            <button onClick={onSubmit}
-            className="block bg-red-500 rounded-[10px] p-2 hover:opacity-80 focus:opacity-80">
-                Submit
-            </button>
+                <button onClick={onSubmit}
+                className="block bg-red-500 text-white rounded-[10px] p-2 hover:opacity-80 focus:opacity-80">
+                    Submit
+                </button>
+            </div>
         </div>
+        
     )
 }
--- src/app/resetPassword/page.tsx
+++ src/app/resetPassword/page.tsx
@@ -37,29 +37,32 @@ export default function ResetPasswordPage(){
     }, []);
 
     return(
-        <div className="w-4/5 mx-auto my-[100px] bg-gradient-to-br from-pink-100 via-white to-blue-200">
-            <div><Toaster/></div>
+        <div className="bg-gradient-to-br from-pink-100 via-white to-blue-200 h-[100vh]">
+            <div className="w-4/5 mx-auto py-[100px] ">
+                <div><Toaster/></div>
 
-            <label htmlFor="email" className="block mb-3">New Password :</label>
-            <input type="password" className="p-2 w-2/5 border-black border-[3px] rounded-lg mb-4 " 
-            onChange={(e)=>setPassword(e.target.value)}
-            value={password}
-            id="email"
-            name="email" 
-            required/>
-            
-            <label htmlFor="email" className="block mb-3">Confirm Password :</label>
-            <input type="password" className="p-2 w-2/5 border-black border-[3px] rounded-lg mb-4 " 
-            onChange={(e)=>setConfirmPassword(e.target.value)}
-            value={confirmPassword}
-            id="email"
-            name="email" 
-            required/>
+                <label htmlFor="email" className="block mb-3">New Password :</label>
+                <input type="password" className="p-2 w-2/5 border-black border-[3px] rounded-lg mb-4 " 
+                onChange={(e)=>setPassword(e.target.value)}
+                value={password}
+                id="email"
+                name="email" 
+                required/>
+                
+                <label htmlFor="email" className="block mb-3">Confirm Password :</label>
+                <input type="password" className="p-2 w-2/5 border-black border-[3px] rounded-lg mb-4 " 
+                onChange={(e)=>setConfirmPassword(e.target.value)}
+                value={confirmPassword}
+                id="email"
+                name="email" 
+                required/>
 
-            <button onClick={onSubmit}
-            className="block bg-gray-700 rounded-[10px] p-2 hover:opacity-80 focus:opacity-80">
-                Submit
-            </button>
+                <button onClick={onSubmit}
+                className="block bg-gray-700 text-white rounded-[10px] p-2 hover:opacity-80 focus:opacity-80">
+                    Submit
+                </button>
+            </div>
         </div>
+        
     )
 }
